1bf4b99d8795bdb50f20f8883ac43817ee28cea2
zhouyi <kkakoz@163.com>
2022-06-10 18:37:07 +0800

    fix:merge

--- internal/repo/user.go
+++ internal/repo/user.go
@@ -10,52 +10,8 @@ type UserRepo struct {
 	ormx.IRepo[model.User]
 }
 
-<<<<<<< HEAD
-func NewUserRepo() domain.IUserRepo {
-	return &UserRepo{}
-}
-
-func (u *UserRepo) WithId(id int64) gormx.DBOption {
-	return func(db *gorm.DB) *gorm.DB {
-		return db.Where("id = ?", id)
-	}
-}
-
-func (u *UserRepo) WithIds(ids []int64) gormx.DBOption {
-	return func(db *gorm.DB) *gorm.DB {
-		return db.Where("id in ?", ids)
-	}
-}
-
-func (u *UserRepo) Add(ctx context.Context, user *domain.User) error {
-	db := mysqlx.GetDB(ctx)
-	err := db.Create(user).Error
-	return errors.Wrap(err, "添加失败")
-}
-
-func (u *UserRepo) Get(ctx context.Context, opts ...gormx.DBOption) (*domain.User, error) {
-	db := mysqlx.GetDB(ctx)
-	user := &domain.User{}
-	for _, opt := range opts {
-		db = opt(db)
-	}
-	err := db.First(user).Error
-	return user, errors.Wrap(err, "查询失败")
-}
-
-func (u *UserRepo) GetList(ctx context.Context, opts ...gormx.DBOption) ([]*domain.User, error) {
-	db := mysqlx.GetDB(ctx)
-	list := make([]*domain.User, 0)
-	for _, opt := range opts {
-		db = opt(db)
-	}
-	err := db.Find(&list).Error
-	return list, errors.Wrap(err, "查询失败")
-}
-=======
 func NewUserRepo() *UserRepo {
 	return &UserRepo{
 		ormx.NewRepo[model.User](),
 	}
 }
->>>>>>> eb83ab769aa25fe40b1cffa3e54381d191dee291
